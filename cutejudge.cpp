9ceaeb59f3a4a9ed5fd35d278dace1eb3cb1525f
nawuraa <nauraazura33@gmail.com>
2024-10-06 21:00:03 +0700

    Add files via upload

---
+++ cutejudge.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "cutejudge.h"
+using namespace std;
+
+void cutejudge() {
+    cout << "          *         * " << endl;
+    cout << "       *     *   *      * " << endl;
+    cout << "       *  *  *  *  *  * *" << endl;
+    cout << "       *                * " << endl;
+    cout << "       *    *     *     * " << endl;
+    cout << "       *                * " << endl;
+    cout << "       *  *         *   *" << endl;
+    cout << "       *   *  *  * *    *" << endl;
+    cout << "       *                *" << endl;
+    cout << "        *              * " << endl;
+    cout << "         *   *   *   *  " << endl; 
+}
